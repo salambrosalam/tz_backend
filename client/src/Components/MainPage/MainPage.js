e65510fdc530300f622644ae52c6c9e7722e0823
Maksym Vasylenko <maximvasylenko228322@gmail.com>
2020-12-03 22:09:54 +0100

    Fix

--- client/src/Components/MainPage/MainPage.js
+++ client/src/Components/MainPage/MainPage.js
@@ -8,7 +8,66 @@ const MainPage = props => {
     const [tasks, setTasks] = useState();
     const [currentFilter, setCurrentFilter] = useState("default");
 
-    const taskState = useSelector(state => state.task.tasks !== null ? state.task.tasks : "not yet");
+    const taskState = useSelector(state => {
+        if (!state.task?.tasks) return null;
+        const usersSet = new Set();
+        const tasks = state.task?.tasks?.issues;
+        tasks.forEach(task => {
+            usersSet.add(task.fields.assignee.displayName)
+        });
+
+        const statuses = {
+            backlog: [],
+            done: [],
+            inProgress: [],
+            selected: [],
+        }
+
+        const res = Object.assign(...Array.from(usersSet, v => ({[v]:{
+            bug: [], story: [], epic: [],
+            }})));
+
+        tasks.forEach(task => {
+            const name = task.fields.assignee.displayName;
+            const tempType = task.fields.issuetype.name;
+            let issueType = '';
+            switch (tempType) {
+                case 'Баг':
+                    issueType = 'bug';
+                    break;
+
+                case 'История':
+                    issueType = 'story';
+                    break;
+
+                case 'Эпик':
+                    issueType = 'epic';
+                    break;
+
+                default: return 'bug';
+            }
+            const tempStatus = task.fields.status.name;
+
+            let status = '';
+            switch (tempStatus) {
+                case 'Backlog':
+                    status = 'backlog';
+                    break;
+
+                case 'В работе':
+                    status = 'inProgress';
+                    break;
+
+                case 'Selected for Development':
+                    status = 'selected';
+                    break;
+
+                default: status = 'done';
+            }
+            res[name][issueType].push({...task, status });
+        });
+        console.log(res);
+    });
 
     const dispatch = useDispatch();
 
@@ -35,7 +94,7 @@ const MainPage = props => {
                 </div>
                 <Board filter={currentFilter}/>
                 I am MainPage that's OK)))
-                <div>{taskState}</div>
+                {/*<div>{taskState}</div>*/}
             </div>
         </div>
     )
